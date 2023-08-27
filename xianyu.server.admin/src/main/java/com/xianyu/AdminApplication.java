a5751db74d560b5823b73072ee20045094191d05
yangfuxing <983276928@qq.com>
2023-08-27 15:25:49 +0800

    -- spring boot 降级 3.0.2 & 增加nacos --

--- xianyu.server.admin/src/main/java/com/xianyu/AdminApplication.java
+++ xianyu.server.admin/src/main/java/com/xianyu/AdminApplication.java
@@ -4,8 +4,13 @@ package com.xianyu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 
+
+@RefreshScope
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AdminApplication {
 
